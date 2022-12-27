1bbb3eb8b81461e5399fc068d8f680cdbff702f4
Julien <61233505+dev-cetus@users.noreply.github.com>
2022-12-27 11:19:37 +0100

    :bug: remove scroll

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex min-h-screen flex-col items-center justify-center bg-slate-800 py-2 sm:px-6 lg:px-8"
+    class="flex h-[calc(100vh-4rem)] flex-col items-center justify-center bg-slate-800 py-2 sm:px-6 lg:px-8"
   >
     <!-- Sign up box -->
     <div
