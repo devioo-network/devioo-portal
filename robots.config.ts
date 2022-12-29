0250304b97ac7e8e6d9185a29ca1107be8b75b13
Julien <61233505+dev-cetus@users.noreply.github.com>
2022-12-29 23:11:50 +0100

    :art: format file

--- robots.config.ts
+++ robots.config.ts
@@ -1,8 +1,11 @@
 import { NodeIncomingMessage } from "h3";
 
 export default [
-  { UserAgent: '*' },
-  { Disallow: '/api' },
+  { UserAgent: "*" },
+  { Disallow: "/api" },
   { BlankLine: true },
-  { Sitemap: (req: NodeIncomingMessage) => `https://${req.headers.host}/sitemap.xml` }
-]
+  {
+    Sitemap: (req: NodeIncomingMessage) =>
+      `https://${req.headers.host}/sitemap.xml`,
+  },
+];
